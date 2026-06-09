a9091ee7dc8326c81084e0d142fbe8b67da381b2
unknown <saravananharish143@gmail.com>
2026-06-09 14:59:28 +0530

    chore: remove backend scratch scripts and purge logs from version control

--- apps/api/check-db.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const projects = await prisma.project.findMany({ include: { milestones: true, tasks: true } });
-  console.log(JSON.stringify(projects, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
--- apps/api/check-users.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const users = await prisma.user.findMany({ select: { email: true } });
-  console.log('Users in DB:', users.map(u => u.email));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
